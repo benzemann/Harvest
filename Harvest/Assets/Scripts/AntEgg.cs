ac550e55df62f7d55db693ba318b72df490ccd43
Benzemann <jeppe_bentzen@hotmail.com>
2017-02-26 18:35:05 +0100

    UI

--- Harvest/Assets/Scripts/AntEgg.cs
+++ Harvest/Assets/Scripts/AntEgg.cs
@@ -5,15 +5,19 @@ using UnityEngine;
 [RequireComponent(typeof(ResourceStorage))]
 public class AntEgg : MonoBehaviour {
 
-    [SerializeField, Tooltip("The time until the egg hatches")]
+    [SerializeField, Tooltip("The time until the egg hatches. This time starts when it is filled with resource and readytime is done")]
     private float hatchTime;
+    [SerializeField, Tooltip("The time until the egg is ready. This time needs to go before hatch time.")]
+    private float readyTime;
     [SerializeField, Tooltip("How much resource is needed for the egg to hatch")]
     private int resourceNeeded;
     [SerializeField, Tooltip("The prefab of the ant that will spawn from this egg")]
     private GameObject ant;
 
+    private bool isReady;
     private ResourceStorage storage;
     private float timeAtInstatiation;
+    private float timeAtReady;
 	// Use this for initialization
 	void Start () {
         ObjectManager.Instance.AddEgg(this.gameObject);
@@ -24,12 +28,16 @@ public class AntEgg : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 
-		if(storage.CurrentStorage >= resourceNeeded && Time.time - timeAtInstatiation >= hatchTime)
+		if(storage.CurrentStorage >= resourceNeeded && Time.time - timeAtInstatiation >= readyTime)
+        {
+            isReady = true;
+            timeAtReady = Time.time;
+        }
+        if (isReady && Time.time - timeAtReady >= hatchTime)
         {
             Instantiate(ant, this.transform.position, this.transform.rotation);
             Destroy(this.gameObject);
         }
-
     }
 
     private void OnDestroy()

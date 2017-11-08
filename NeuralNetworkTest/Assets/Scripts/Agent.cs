1f28b1e888ac39cceba27a9be02ac1e9df3a70f8
Jeppe Bentzen <bentzen.jeppe@gmail.com>
2017-11-08 22:14:26 +0100

    Added NeuralNetworkTest

---
+++ NeuralNetworkTest/Assets/Scripts/Agent.cs
@@ -0,0 +1,136 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Agent : MonoBehaviour {
+
+    NeuralNetwork network;
+    bool initialized = false;
+    Rigidbody rb;
+
+    [SerializeField]
+    Transform sensorRight;
+    [SerializeField]
+    Transform sensorLeft;
+
+	// Use this for initialization
+	void Start () {
+        rb = GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (initialized)
+        {
+            var input = new float[4];
+
+            var angle = SignedAngleBetween(this.transform.forward, (Manager.Instance.PosOfClosestRes(this.transform.position) - this.transform.position).normalized, Vector3.up);
+
+            input[0] = angle;
+            input[1] = 1;
+            input[2] = 1;
+            input[3] = 1;
+
+            RaycastHit hit;
+            Ray ray = new Ray(this.transform.position + (Vector3.up * 0.25f), sensorRight.position - (this.transform.position + (Vector3.up * 0.25f)));
+            Debug.DrawRay(this.transform.position + (Vector3.up * 0.25f), (sensorRight.position - (this.transform.position + (Vector3.up * 0.25f))).normalized, Color.blue);
+            if (Physics.Raycast(ray, out hit, 1f))
+            {
+                if(hit.transform.gameObject != this.gameObject && hit.transform.GetComponent<Res>() != null)
+                {
+                    input[1] = -1;
+                } 
+            }
+            
+            ray = new Ray(this.transform.position + (Vector3.up * 0.25f), sensorLeft.position - (this.transform.position + (Vector3.up * 0.25f)));
+            Debug.DrawRay(this.transform.position + (Vector3.up * 0.25f), (sensorLeft.position - (this.transform.position + (Vector3.up * 0.25f))).normalized, Color.red);
+            if (Physics.Raycast(ray, out hit, 1f))
+            {
+                if (hit.transform.gameObject != this.gameObject && hit.transform.GetComponent<Res>() != null)
+                {
+                    input[2] = -1;
+                }
+            }
+            
+            ray = new Ray(this.transform.position + (Vector3.up * 0.25f), this.transform.forward);
+           // Debug.DrawRay(this.transform.position + (Vector3.up * 0.25f), this.transform.forward, Color.cyan);
+            if (Physics.Raycast(ray, out hit, 1f))
+            {
+                if (hit.transform.gameObject != this.gameObject && hit.transform.GetComponent<Res>() != null)
+                {
+                    input[3] = -1;
+                }
+            }
+
+            var output = network.FeedForward(input);
+
+            var vel = output[0];
+
+            if (vel < -0.2f)
+                vel = -0.2f;
+
+            rb.velocity = this.transform.forward * vel * Time.deltaTime * 500f;
+            rb.AddTorque(this.transform.up * Time.deltaTime * output[1] * 500f, ForceMode.VelocityChange);
+
+            ray = new Ray(this.transform.position + (Vector3.up * 0.25f), this.transform.forward);
+            Debug.DrawRay(this.transform.position + (Vector3.up * 0.25f), this.transform.forward * 0.75f, Color.cyan);
+            if (Physics.Raycast(ray, out hit, 0.75f))
+            {
+                if (hit.transform.gameObject != this.gameObject && hit.transform.GetComponent<Res>() != null)
+                {
+                    network.AddFitness(-0.1f);
+                }
+            }
+
+            //var fitness = 1.0f - (Mathf.Min(10.0f, Manager.Instance.DistanceToClosestRes(this.transform.position)) / 10f);
+            //network.AddFitness(fitness);
+        }
+        /*
+        if (this.transform.position.x > 50.0f)
+            this.transform.position = new Vector3(0f, this.transform.position.y, 0f);
+        if (this.transform.position.x < -50.0f)
+            this.transform.position = new Vector3(0f, this.transform.position.y, 0f);
+
+        if (this.transform.position.z > 50.0f)
+            this.transform.position = new Vector3(0f, this.transform.position.y, 0f);
+
+        if (this.transform.position.z < -50.0f)
+            this.transform.position = new Vector3(0f, this.transform.position.y, 0f);
+            */
+    }
+
+    public void Init(NeuralNetwork n)
+    {
+        network = n;
+        initialized = true;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.GetComponent<Res>() != null)
+        {
+            other.GetComponent<Res>().Delete();
+            network.AddFitness(1f);
+        }
+    }
+
+    public void SetNeuralNetwork(NeuralNetwork n)
+    {
+        network = n;
+    }
+
+    float SignedAngleBetween(Vector3 a, Vector3 b, Vector3 n)
+    {
+        // angle in [0,180]
+        float angle = Vector3.Angle(a, b);
+        float sign = Mathf.Sign(Vector3.Dot(n, Vector3.Cross(a, b)));
+
+        // angle in [-179,180]
+        float signed_angle = angle * sign;
+
+        // angle in [0,360] (not used but included here for completeness)
+        //float angle360 =  (signed_angle + 180) % 360;
+
+        return signed_angle;
+    }
+}
---
+++ NeuralNetworkTest/Assets/Scripts/Manager.cs
@@ -0,0 +1,152 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Manager : Singleton<Manager> {
+    [SerializeField]
+    GameObject agentPrefab;
+    [SerializeField]
+    GameObject resPrefab;
+    [SerializeField]
+    Text text;
+
+    float timeAtLastSpawn;
+    float timeAtLastGeneration;
+    List<GameObject> allRes;
+    List<GameObject> allAgents;
+    int currentGeneration = 0;
+    int populationSize;
+    int[] layers = new int[] { 4, 10, 10, 2 };
+    List<NeuralNetwork> nets;
+
+    // Parameters
+    [SerializeField]
+    int startAgents;
+    [SerializeField]
+    int startRes;
+    [SerializeField]
+    float resSpawnRate;
+    [SerializeField]
+    float generationTime;
+
+	// Use this for initialization
+	void Start () {
+        // Init lists
+        allRes = new List<GameObject>();
+        allAgents = new List<GameObject>();
+        nets = new List<NeuralNetwork>();
+        // Create start res and agents
+        for (int i = 0; i < startRes; i++)
+            CreateRes();
+        for (int i = 0; i < startAgents; i++)
+            CreateAgent();
+
+        timeAtLastGeneration = Time.time;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        populationSize = allAgents.Count;
+		// Spawn res
+        if (Time.time - timeAtLastSpawn >= resSpawnRate)
+        {
+            CreateRes();
+            timeAtLastSpawn = Time.time;
+        }
+        // New generation
+        if(Time.time - timeAtLastGeneration >= generationTime)
+        {
+            NewGeneration();
+            timeAtLastGeneration = Time.time;
+        }
+
+        UpdateDebugText();
+	}
+
+    void NewGeneration()
+    {
+        nets.Sort();
+        for (int i = 0; i < populationSize / 2 ; i++)
+        {
+            nets[i] = new NeuralNetwork(nets[i + (populationSize / 2)], nets[i].Agent);
+            nets[i].Mutate();
+            nets[i + (populationSize / 2)] = new NeuralNetwork(nets[i + (populationSize / 2)], nets[i + (populationSize / 2)].Agent);
+        }
+
+        for (int i = 0; i < populationSize; i++)
+        {
+            nets[i].SetFitness(0f);
+        }
+
+        currentGeneration++;
+    }
+
+    void CreateAgent()
+    {
+        if (agentPrefab == null)
+            return;
+        var agent = Instantiate(agentPrefab, new Vector3(Random.Range(-49f, 49f), 0.0f, Random.Range(-49f, 49f)), Quaternion.identity) as GameObject;
+        allAgents.Add(agent);
+        var network = new NeuralNetwork(layers);
+        agent.GetComponent<Agent>().Init(network);
+        network.Agent = agent;
+        nets.Add(network);
+    }
+
+    void CreateRes()
+    {
+        if (resPrefab == null || allRes.Count >= 100)
+            return;
+        allRes.Add(Instantiate(resPrefab, new Vector3(Random.Range(-49f, 49f), 0.25f, Random.Range(-49f, 49f)), Quaternion.identity) as GameObject);
+    }
+
+    public float DistanceToClosestRes(Vector3 pos)
+    {
+        float closestDis = float.MaxValue;
+        for(int i = 0; i < allRes.Count; i++)
+        {
+            var dis = Vector3.Distance(pos, allRes[i].transform.position);
+            if(dis < closestDis)
+            {
+                closestDis = dis;
+            }
+        }
+        return closestDis;
+    }
+
+    public Vector3 PosOfClosestRes(Vector3 pos)
+    {
+        float closestDis = float.MaxValue;
+        Vector3 closestPos = closestPos = Vector3.zero;
+        for (int i = 0; i < allRes.Count; i++)
+        {
+            var dis = Vector3.Distance(pos, allRes[i].transform.position);
+            if (dis < closestDis)
+            {
+                closestDis = dis;
+                closestPos = allRes[i].transform.position;
+            }
+        }
+        return closestPos;
+    }
+
+    public void DeleteRes(GameObject res)
+    {
+        if (allRes.Contains(res))
+        {
+            allRes.Remove(res);
+            Destroy(res);
+        }
+    }
+
+    void UpdateDebugText()
+    {
+        var t = "Neural network info:" + System.Environment.NewLine;
+        t += "Generation: " + currentGeneration + System.Environment.NewLine;
+        t += "Population: " + populationSize + System.Environment.NewLine;
+        t += "Time until next generation: " + (generationTime - (Time.time - timeAtLastGeneration)) + System.Environment.NewLine;
+        t += "Resources: " + allRes.Count;
+        text.text = t;
+    }
+}
